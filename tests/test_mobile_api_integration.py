bca36ba4ced167be4e74df25363cf09600c0d4de
Moran Liv <moran.liv2006@gmail.com>
2025-12-25 13:11:29 +0200

    Styling

--- tests/test_mobile_api_integration.py
+++ tests/test_mobile_api_integration.py
@@ -9,7 +9,8 @@ class TestMobileApiIntegration:
 
     @pytest.mark.integration
     @pytest.mark.parametrize("mobile_session", ["ios", "android"], indirect=True)
-    def test_stream_status_is_consistent_in_both_the_api_and_the_mobile_layers(self, mobile_session, streaming_validator, credentials):
+    def test_stream_status_is_consistent_in_both_the_api_and_the_mobile_layers(self, mobile_session, streaming_validator,
+                                                                               credentials):
         # mobile section
         welcome = WelcomeScreen(mobile_session)
         login = LoginScreen(mobile_session)
